9f145488be8905dc4425abfbcd29b1132c6409f2
Brock Riemenschneider <brock@mvpdesign.com>
2014-12-01 12:03:27 -0600

    Added generate random string to helper

--- spec/MVPDesign/ThemosisInstaller/HelperSpec.php
+++ spec/MVPDesign/ThemosisInstaller/HelperSpec.php
@@ -8,6 +8,20 @@ use Prophecy\Argument;
 class HelperSpec extends ObjectBehavior
 {
     /**
+     * get matches
+     *
+     * @return array
+     */
+    public function getMatchers()
+    {
+        return array(
+            'haveLength' => function ($subject, $length) {
+                return strlen($subject) === $length;
+            }
+        );
+    }
+
+    /**
      * it is initializable
      *
      * @return void
@@ -15,5 +29,18 @@ class HelperSpec extends ObjectBehavior
     public function it_is_initializable()
     {
         $this->shouldHaveType('MVPDesign\ThemosisInstaller\Helper');
+    }
+
+    /**
+     * it should generate random strings
+     *
+     * @return void
+     */
+    public function it_should_generate_random_strings()
+    {
+        $this->generateRandomString(8)->shouldHaveLength(8);
+        $this->generateRandomString(16)->shouldHaveLength(16);
+        $this->generateRandomString(32)->shouldHaveLength(32);
+        $this->generateRandomString()->shouldHaveLength(64);
     }
 }
--- src/MVPDesign/ThemosisInstaller/Helper.php
+++ src/MVPDesign/ThemosisInstaller/Helper.php
@@ -4,4 +4,24 @@ namespace MVPDesign\ThemosisInstaller;
 
 class Helper
 {
+    /**
+     * generate a random string of characters
+     *
+     * @param  string $length
+     * @return string
+     */
+    public static function generateRandomString($length = 64)
+    {
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $chars .= '!@#$%^&*()';
+        $chars .= '-_ []{}<>~`+=,.;:/?|';
+
+        $string = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $string .= substr($chars, rand(0, strlen($chars) - 1), 1);
+        }
+
+        return $string;
+    }
 }
