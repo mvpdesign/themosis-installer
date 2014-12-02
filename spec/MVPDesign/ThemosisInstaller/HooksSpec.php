c5eacefb2b7e677b19d04d592dd1154c9f6815d8
Brock Riemenschneider <brock@mvpdesign.com>
2014-12-02 13:52:00 -0600

    Added themosis hook to hooks

--- spec/MVPDesign/ThemosisInstaller/HooksSpec.php
+++ spec/MVPDesign/ThemosisInstaller/HooksSpec.php
@@ -4,6 +4,7 @@ namespace spec\MVPDesign\ThemosisInstaller;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Mockery;
 
 class HooksSpec extends ObjectBehavior
 {
@@ -15,5 +16,22 @@ class HooksSpec extends ObjectBehavior
     public function it_is_initializable()
     {
         $this->shouldHaveType('MVPDesign\ThemosisInstaller\Hooks');
+    }
+
+    /**
+     * it should have a hook for themosis
+     *
+     * @return void
+     */
+    public function it_should_have_a_hook_for_themosis()
+    {
+        $io = Mockery::mock('Composer\IO\BaseIO')
+            ->shouldReceive('isInteractive', 'write')
+            ->andReturn(false)->getMock();
+        $event = Mockery::mock('Composer\Script\Event')
+            ->shouldReceive('getIO')
+            ->andReturn($io)->getMock();
+
+        $this->shouldThrow(new \MVPDesign\ThemosisInstaller\InvalidStringLengthException("The string must have a length greater than 0."))->duringThemosis($event);
     }
 }
--- src/MVPDesign/ThemosisInstaller/Hooks.php
+++ src/MVPDesign/ThemosisInstaller/Hooks.php
@@ -2,7 +2,23 @@
 
 namespace MVPDesign\ThemosisInstaller;
 
+use Composer\Script\Event;
+use MVPDesign\ThemosisInstaller\Themosis;
+
 class Hooks
 {
+    /**
+     * the themosis install hook
+     *
+     * @param  Event $event
+     * @return void
+     */
+    public static function themosis(Event $event)
+    {
+        $io = $event->getIO();
 
+        $themosis = new Themosis($io);
+        $themosis->askConfigQuestions();
+        $themosis->install();
+    }
 }
