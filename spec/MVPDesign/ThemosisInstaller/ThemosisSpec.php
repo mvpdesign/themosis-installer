024d829573372f6a6a2196699d0b6f68c4b98735
Brock Riemenschneider <brock@mvpdesign.com>
2014-12-02 10:41:56 -0600

    Added iointerface to themosis

--- spec/MVPDesign/ThemosisInstaller/ThemosisSpec.php
+++ spec/MVPDesign/ThemosisInstaller/ThemosisSpec.php
@@ -4,9 +4,24 @@ namespace spec\MVPDesign\ThemosisInstaller;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Mockery;
 
 class ThemosisSpec extends ObjectBehavior
 {
+    /**
+     * let
+     *
+     * @return void
+     */
+    public function let()
+    {
+        $io = Mockery::mock('Composer\IO\BaseIO')
+            ->shouldReceive('isInteractive', 'write')
+            ->andReturn(false)->getMock();
+
+        $this->beConstructedWith($io);
+    }
+
     /**
      * it is initializable
      *
--- src/MVPDesign/ThemosisInstaller/Themosis.php
+++ src/MVPDesign/ThemosisInstaller/Themosis.php
@@ -2,6 +2,22 @@
 
 namespace MVPDesign\ThemosisInstaller;
 
+use Composer\IO\IOInterface;
+
 class Themosis
 {
+    /**
+     * io interface
+     *
+     * @var IOInterface
+     */
+    private $io;
+
+    /**
+     * constructor
+     */
+    public function __construct(IOInterface $io)
+    {
+        $this->io = $io;
+    }
 }
