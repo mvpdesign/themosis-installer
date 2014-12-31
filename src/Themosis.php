992d2081f09a59be89f7b8c5ecf6b5c762636f65
Zach Bosek <zach@mvpdesign.com>
2014-12-31 11:31:42 -0600

    fix syntax error

--- src/Themosis.php
+++ src/Themosis.php
@@ -767,7 +767,7 @@ class Themosis
         //'rm -R codeception'
         //'vendor/bin/codecept generate:cept acceptance Home'
  
-        $command = 'cd ' . $this->retrieveThemosisThemePath() . ' && vendor/bin/codecept bootstrap && cp codeception-config.yml codeception.yml && cp -r tests codeception && rm -R tests && mkdir tests && cp -r codeception tests/codeception && rm -R codeception && vendor/bin/codecept generate:cept acceptance Home'
+        $command = 'cd ' . $this->retrieveThemosisThemePath() . ' && vendor/bin/codecept bootstrap && cp codeception-config.yml codeception.yml && cp -r tests codeception && rm -R tests && mkdir tests && cp -r codeception tests/codeception && rm -R codeception && vendor/bin/codecept generate:cept acceptance Home';
         
         $this->runProcess($command, 'Initiated PhpSpec.', false, true);
 
